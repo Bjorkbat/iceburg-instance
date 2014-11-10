c41b9b012b8282e49072778a06e0f99bfe1c2254
Bjorkbat <bjorkbat@gmail.com>
2014-11-10 01:00:23 -0700

    added additional requirements in order to run main game

--- public/js/iceburg.js
+++ public/js/iceburg.js
@@ -1,20 +1,43 @@
 require.config({
   paths: {
+    // Shared game scripts
     "text": "/public/js/text",
     "ThreeJS": "/public/js/three.min",
     "Stats": "/public/js/stats",
+
+    // The intro
     "Intro": "/public/js/intro",
+    "UI": "/public/js/ui",
     "IntroVertex": "/public/glsl/IntroVertex.glsl",
-    "IntroFragment": "/public/glsl/IntroFragment.glsl"
+    "IntroFragment": "/public/glsl/IntroFragment.glsl",
+
+    // The game proper
+    "Game": "/public/js/game",
+    "PointerLock": "/public/js/pointerlockcontrols",
+    "RaycastRifle": "/public/js/raycastrifle",
+    "FractalFlora": "/public/js/fractalflora",
+    "Tetrademon": "/public/js/tetrademon",
+    "Terrain": "/public/js/terrain",
+    "TerraVertex": "/public/glsl/terravertex.glsl",
+    "TerraFragment": "/public/glsl/terrafragment.glsl"
   }
 });
 
 require(["ThreeJS", "Stats"], function() {
+
   require(
-    ["text!IntroVertex", "text!IntroFragment", "Intro"],
+    ["text!IntroVertex", "text!IntroFragment", "Intro", "UI"],
     function(introVertex, introFragment, intro) {
       init(introVertex, introFragment);
       return;
     }
   );
+
+  require(
+    ["text!TerraVertex", "text!TerraFragment", "Game", "Terrain",
+    "PointerLock", "RaycastRifle", "FractalFlora", "Tetrademon"],
+    function(terraVertex, terraFragment, Game) {
+      assignVertexes(terraVertex, terraFragment);
+    }
+  )
 });
