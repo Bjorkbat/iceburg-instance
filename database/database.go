233f83086d33107f6b2906092366facdcecdfd6f
Bjorkbat <bjorkbat@gmail.com>
2014-11-10 02:47:42 -0700

    added beginnings of database wrapper

---
+++ database/database.go
@@ -0,0 +1,43 @@
+/*
+ * A wrapper for the database.
+ * * * * */
+
+package database
+
+import (
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
+
+  "github.com/iceburg-instance/database/db_settings"
+)
+
+var db *sql.DB
+
+// Self-explanatory, open a connection, but store the db as a global variable,
+// that way parts of the code that need the database can simply call the wrapper
+// rather than awkwardly passing around a pointer to the db
+func Open() error {
+
+  var err error
+  settings := db_settings.GetSettings()
+  db, err = sql.Open(settings.DBType, settings.Username + ":" +
+    settings.Passwd + "@/" + settings.DBName)
+  if err != nil {
+    return err
+  }
+
+  // No error, so now let's ping the database and see what happens
+  err = db.Ping()
+  if err != nil {
+    return err
+  }
+
+  // No error, then return nil and be happy
+  return nil
+}
+
+// Simple
+func Close() {
+  db.Close()
+  return
+}
