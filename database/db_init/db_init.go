1796ff524a119c358a420915dfdd00a8ec7f3cd9
Bjorkbat <bjorkbat@gmail.com>
2014-11-10 03:08:50 -0700

    added package for initialization of database

---
+++ database/db_init/db_init.go
@@ -0,0 +1,46 @@
+/*
+ * Package used for initializing a fresh database, or for syncing it
+ * * * * */
+
+package db_init
+
+import (
+
+  "database/sql"
+  "fmt"
+  _ "github.com/go-sql-driver/mysql"
+
+  "github.com/iceburg-instance/database/db_settings"
+  "github.com/iceburg-instance/database/models/user"
+)
+
+// Initializes a fresh database.  Also used to reset the database if necessary
+// (note,not for long)
+func Init() error {
+  // first, get the settings
+  settings := db_settings.GetSettings()
+  // Open the connection
+  db, err := sql.Open(settings.DBType, settings.Username + ":" +
+    settings.Passwd + "@/" + settings.DBName)
+
+  if err != nil {
+    return err
+  }
+  defer db.Close()
+
+  // Ping to make sure it's really open
+  err = db.Ping()
+  if err != nil {
+    return err
+  }
+
+  // Finally, create the necessary tables for the models
+  qString := user.GetInit()
+  _, err = db.Exec(qString)
+  if err != nil {
+    return err
+  }
+  fmt.Println("Added user table")
+
+  return nil
+}
