795b3aad35db1602501a7844acbb50c73aa2fd00
Bjorkbat <bjorkbat@gmail.com>
2014-11-11 04:43:50 -0700

    no idea

--- database/models/user/user.go
+++ database/models/user/user.go
@@ -7,7 +7,7 @@ package user
 func GetInit() string {
   var qString string = "CREATE TABLE IF NOT EXISTS user (" +
     "id INTEGER AUTO_INCREMENT PRIMARY KEY, " +
-    "username VARCHAR(48) NOT NULL UNIQUE, " +
+    "username VARCHAR(32) NOT NULL UNIQUE, " +
     "email VARCHAR(255) NOT NULL UNIQUE, " +
     "password VARCHAR(44) NOT NULL, " +
     "salt VARCHAR(44) NOT NULL )"
