736f7903ae7497783814adfbec7440ff5307feff
Bjorkbat <bjorkbat@gmail.com>
2014-11-11 14:02:51 -0700

    added handy dandy UserModel struct

--- database/models/user/user.go
+++ database/models/user/user.go
@@ -4,10 +4,16 @@
 
 package user
 
+type UserModel struct {
+  Username string
+  Email string
+  Password string
+  Salt string
+}
+
 func GetInit() string {
   var qString string = "CREATE TABLE IF NOT EXISTS user (" +
-    "id INTEGER AUTO_INCREMENT PRIMARY KEY, " +
-    "username VARCHAR(32) NOT NULL UNIQUE, " +
+    "username VARCHAR(32) PRIMARY KEY, " +
     "email VARCHAR(255) NOT NULL UNIQUE, " +
     "password VARCHAR(44) NOT NULL, " +
     "salt VARCHAR(44) NOT NULL )"
@@ -24,4 +30,13 @@ func GenInsert(username string, email string, saltedpass string, salt string) st
     "'" + salt + "' )"
 
   return insertString
+}
+
+// Looking past how absurd the name is, what this function does is return a
+// string that grabs only one model from the database using the param
+// included as arg (will expand as see fit)
+// Really don't need to retrieve ALL the things.  Blast it
+func GetGet(column string) string {
+  var qString string = "SELECT * FROM user WHERE " + column + " =?"
+  return qString
 }
