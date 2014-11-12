c3c8cb2660bced017972c95d5d7d56f4124225c6
Bjorkbat <bjorkbat@gmail.com>
2014-11-12 00:16:26 -0700

    added new package for game assets, including code for retrieving terrain height data

---
+++ assets/assets.go
@@ -0,0 +1,6 @@
+/*
+ * package for loading game assets stored in the database somewhere, things
+ * like terrain height data, trees, monsters, etc
+ * * * * */
+
+package assets
---
+++ assets/terrain.go
@@ -0,0 +1,45 @@
+/*
+ * Used to retrieve data relating to the terrain
+ * * * * */
+
+package assets
+
+import (
+  "fmt"
+
+  "github.com/iceburg-instance/database"
+  "github.com/iceburg-instance/database/models/terrain"
+)
+
+type HeightData struct {
+  HeightVals []HeightTuple
+}
+
+// Extract the terrain height data, return it as a JSON object
+func GetHeight() *HeightData {
+
+  // Get all 5000 something rows of vertice height vals and shove them into
+  // an array of HeightTuples
+  qString := terrain.GetAllString()
+  rows, err := database.Query(qString)
+  if err != nil {
+    fmt.Println(err)
+    return err
+  }
+  defer rows.Close()
+
+  var heightSlice []terrain.HeightTuple
+  var heightTuple terrain.HeightTuple
+
+  for rows.Next() {
+    heightTuple = terrain.HeightTuple{}
+    if err := rows.Scan(&heightTuple.Id, &heightTuple.Height); err != nil {
+      fmt.Println(err)
+      return err
+    }
+    heightSlice = append(heightSlice, heightTuple)
+  }
+
+  // Create new HeightData struct using heightSlice
+  return HeightData{HeightVals: heightSlice}
+}
