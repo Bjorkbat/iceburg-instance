474f322237732557591d88da42e220d403810f1e
Bjorkbat <bjorkbat@gmail.com>
2014-11-12 00:26:00 -0700

    added handler for getting height data and returning a JSON object

--- assets/assets.go
+++ assets/assets.go
@@ -4,3 +4,36 @@
  * * * * */
 
 package assets
+
+import (
+  "encoding/json"
+  "fmt"
+  "net/http"
+)
+
+// Handler for getting terrain data, currenly just returns a JSON object
+// representing the terrain height data
+func TerrainHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "GET" {
+    // Permission error
+    return
+  }
+
+  // Grab terrain height data using GetHeight
+  heightData, err := GetHeight()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Marshal it to a JSON object
+  obj, err := json.Marshal(heightData)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Write the byte data back to client
+  w.Write(obj)
+  return
+}
